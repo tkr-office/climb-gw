d9c7d0b833aedd80087c56cb9f3d736b8268bde1
Takayuki Kubo <TAKATON2002@yahoo.co.jp>
2017-10-21 15:46:44 +0900

    資料追加

---
+++ src/jp/co/is/gw/rating/engine/fire/impl/HouseholdGoodsBasicRatingSpec.java
@@ -0,0 +1,33 @@
+package jp.co.is.gw.rating.engine.fire.impl;
+
+import java.math.BigDecimal;
+
+import jp.co.is.gw.rating.engine.RatingContext;
+import jp.co.is.gw.rating.engine.RatingSpec;
+
+/**
+ * 家財基本料率仕様
+ *
+ * @author t-kubo
+ *
+ */
+public class HouseholdGoodsBasicRatingSpec extends AbstractRatingSpec {
+
+	public HouseholdGoodsBasicRatingSpec(RatingSpec dependRating, RatingContext context) {
+		super(dependRating, context);
+	}
+
+	/**
+	 * 家財基本料率を適用する
+	 *
+	 *
+	 * @return 家財基本料率
+	 */
+	@Override
+	public BigDecimal apply() {
+
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
